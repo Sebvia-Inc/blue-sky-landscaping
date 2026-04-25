206a6d068355bfa9e0ea4d6fadbd2d98066206cb
siddharthmishra95 <siiid.mishra@gmail.com>
2026-04-25 18:06:16 +0530

    final commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
+import lenis from 'astro-lenis'
 import tailwindcss from '@tailwindcss/vite'
 import { client } from './src/client.config.js'
 
@@ -9,6 +10,7 @@ export default defineConfig({
   output: 'static',
   integrations: [
     sitemap(),
+    lenis(),
   ],
   vite: {
     plugins: [tailwindcss()],
